fba3e85a59503527974a9db6bbeab3cd8671b343
n105na <ambenaini@gmail.com>
2025-05-20 01:10:20 +0100

    the mission tab

--- budget_management/missions/admin.py
+++ budget_management/missions/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-from .models import GradePayment
+from .models import GradePayment, Mission, MissionPersonnel
+
 
 @admin.register(GradePayment)
 class GradePaymentAdmin(admin.ModelAdmin):
-    list_display = ('grade', 'meal_payment_north', 'meal_payment_south', 'lodging_payment_north', 'lodging_payment_south')
+    list_display = ("grade", "meal_payment_north", "meal_payment_south", "lodging_payment_north", "lodging_payment_south")
+    search_fields = ("grade__name",)
+    list_filter = ("grade",)
+
+
+@admin.register(Mission)
+class MissionAdmin(admin.ModelAdmin):
+    list_display = ("mission_nature", "destination_wilaya", "transport_type", "funding_type", "date_departure", "date_arrival")
+    list_filter = ("transport_type", "funding_type", "mission_nature", "destination_wilaya")
+    search_fields = ("mission_nature", "destination_wilaya__name")
+
+
+@admin.register(MissionPersonnel)
+class MissionPersonnelAdmin(admin.ModelAdmin):
+    list_display = ("personnel", "mission", "transport_payment", "meal_payment", "lodging_payment", "total_payment")
+    list_filter = ("mission__funding_type", "mission__destination_wilaya", "personnel__grade")
+    search_fields = ("personnel__name", "mission__mission_nature", "mission__destination_wilaya__name")
