6d4b04034c2bc1b1f20303640e22764ea661f211
n105na <ambenaini@gmail.com>
2025-05-20 12:04:10 +0100

    My local work before pull

--- budget_management/budget_management/settings.py
+++ budget_management/budget_management/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'corsheaders',
     'rest_framework.authtoken',
+    'django_filters',
     'users',
     'personnel',
     'locations',
@@ -55,6 +56,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter'
+    ]
 }
 from datetime import timedelta
 SIMPLE_JWT = {
--- budget_management/personnel/views.py
+++ budget_management/personnel/views.py
@@ -4,12 +4,31 @@ from .models import Personnel, Grade, Wilaya
 from .serializers import PersonnelSerializer, GradeSerializer, WilayaSerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter  
+
 
 class PersonnelViewSet(viewsets.ModelViewSet):
     queryset = Personnel.objects.all()
     serializer_class = PersonnelSerializer
     permission_classes = [IsAuthenticated]
-
+    
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    
+    
+    filterset_fields = {
+        'name': ['icontains'],            # filter by partial name match
+        'profession': ['exact'],          # exact match: Teacher, Worker...
+        'grade': ['exact'],               # grade ID
+        'grade__name': ['icontains'],     # grade name contains
+        'wilaya': ['exact'],              # wilaya ID
+        'wilaya__name': ['icontains'],    # wilaya name contains
+        'is_ccp_account': ['exact'],      # true/false
+    }
+    ordering_fields = ['name', 'wilaya__name', 'grade__name']
+   # GET /api/personnel/?ordering=name
+   #GET /api/personnel/?profession=Teacher
+   # GET /api/personnel/?name__icontains=a searching for all name that has a .... 
 class GradeViewSet(viewsets.ModelViewSet):
     queryset = Grade.objects.all()
     serializer_class = GradeSerializer
